891ecd9f5ecbc8291371f2ae235914789132ff42
JOAO PAULO <joao.silva@mgmcorp.com.br>
2021-04-08 16:19:05 -0300

    Add Claim Method Controller

---
+++ src/ICWebAPI/Controllers/ClaimsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Claims;
+
+namespace ICWebAPI.Controllers
+{
+    [Route("[controller]")]
+    public class ClaimsController : MainController
+    {
+        [HttpGet]
+        public IActionResult GetClaims()
+        {
+            if (User.Identity is ClaimsIdentity identity)
+            {
+                var claims = from c in identity.Claims
+                             select new
+                             {
+                                 subject = c.Subject.Name,
+                                 type = c.Type,
+                                 value = c.Value
+                             };
+
+                return Ok(claims);
+            }
+
+            return NotFound();
+        }
+    }
+}
--- src/ICWebAPI/Controllers/RolesController.cs
+++ src/ICWebAPI/Controllers/RolesController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Threading.Tasks;
 
 namespace ICWebAPI.Controllers
