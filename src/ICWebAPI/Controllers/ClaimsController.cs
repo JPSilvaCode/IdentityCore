f15e3b3052174f427b3a4521d9f16deca4ac3041
João Paulo da Silva <joao.silva@mgmcorp.com.br>
2021-04-08 20:50:19 -0300

    Update Claims Controller- GetAll

--- src/ICWebAPI/Controllers/ClaimsController.cs
+++ src/ICWebAPI/Controllers/ClaimsController.cs
@@ -1,29 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ICWebAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace ICWebAPI.Controllers
 {
     [Route("[controller]")]
     public class ClaimsController : MainController
     {
-        [HttpGet]
-        public IActionResult GetClaims()
-        {
-            if (User.Identity is ClaimsIdentity identity)
-            {
-                var claims = from c in identity.Claims
-                             select new
-                             {
-                                 subject = c.Subject.Name,
-                                 type = c.Type,
-                                 value = c.Value
-                             };
+        private readonly UserManager<IdentityUser> _userManager;
 
-                return Ok(claims);
+        public ClaimsController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetClaims()
+        {
+            var users = await _userManager.Users.ToListAsync();
+
+            IList<ClaimBinding> claims = null;
+            foreach (var user in users)
+            {
+                var claimsUser = await _userManager.GetClaimsAsync(user);
+
+                if (claimsUser == null) continue;
+
+                claims ??= new List<ClaimBinding>();
+
+                foreach (var claimUser in claimsUser)
+                {
+                    claims.Add(new ClaimBinding {Type = claimUser.Type, Value = claimUser.Value});
+                }
             }
 
-            return NotFound();
+            if (claims == null) return NotFound();
+
+            //var result = claims.GroupBy(c => c.Type, c => c.Value, (key, g) => new { key}).ToList();
+            var result = claims.GroupBy(c => new {c.Type, c.Value}, (key, g) => new {key, g}).Select(g => new { g.key.Type, g.key.Value }).ToList();
+
+            return Ok(result);
         }
     }
 }
